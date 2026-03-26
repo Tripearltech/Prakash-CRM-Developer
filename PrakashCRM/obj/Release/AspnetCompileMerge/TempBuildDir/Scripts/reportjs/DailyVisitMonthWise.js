70adeb7eadb2069698669307c8937875eeeeb24c
Praduman Prajapati <praduman.p@tripearltech.com>
2026-03-26 14:36:22 +0530

    update code

---
+++ PrakashCRM/obj/Release/AspnetCompileMerge/TempBuildDir/Scripts/reportjs/DailyVisitMonthWise.js
@@ -0,0 +1,127 @@
+﻿var apiUrl = $('#getServiceApiUrl').val() + 'SPReports/';
+
+$(document).ready(function () {
+    var fDate = "";
+    var tDate = "";
+
+    DailyVisitMonthWise(fDate, tDate);
+
+});
+
+function DailyVisitMonthWise(fDate, tDate) {
+    debugger
+    var fromDate = fDate;
+    var toDate = tDate;
+
+    $.ajax({
+        url: '/SPReports/GetDailyVisitMonthWise',
+        type: 'GET',
+        contentType: 'application/json',
+        data: {
+            FromDate: fromDate,
+            ToDate: toDate
+        },
+        success: function (data) {
+            $('#tblDailyVisitMonthWise').empty();
+            var rowData = "";
+
+            if (data.DailyVisitMonthWises && data.DailyVisitMonthWises.length > 0) {
+                $.each(data.DailyVisitMonthWises, function (index, item) {
+
+                    rowData += "<tr><td>" + item.SalesPerson_Name + "</td><td>" + item.Month_Year + "</td><td>" + item.Total_Time_HH_MM + "</td><td>" + item.No_of_Visit_Personal + "</td><td>" + item.Total_Kilometers + "</td></tr>";
+                });
+            }
+            //else {
+            //    rowData = "<tr><td colspan='9' style='text-align:left;'>No Records Found</td></tr>";
+            //}
+            $('#tblDailyVisitMonthWise').append(rowData);
+            $('#tblEmpDailyVisitMonthWise').empty();
+            var employeeRow = "";
+            if (data.EmployeeDailyVisitMonthWise && data.EmployeeDailyVisitMonthWise.length > 0) {
+                $.each(data.EmployeeDailyVisitMonthWise, function (index, item) {
+                    employeeRow += "<tr><td>" + item.SalesPerson_Name + "</td><td>" + item.Month_Year + "</td><td>" + item.Total_Time_HH_MM + "</td><td>" + item.No_of_Visit_Personal + "</td><td>" + item.Total_Kilometers + "</td></tr>";
+                });
+            }
+            $('#tblEmpDailyVisitMonthWise').append(employeeRow);
+
+        },
+
+    });
+
+}
+
+$("#btnClearFilter").on('click', function () {
+
+    ClearDispatchFilter();
+});
+
+function ClearDispatchFilter() {
+    var fDate = "";
+    var tDate = "";
+
+    $("#FromDate").val('');
+    $("#ToDate").val('');
+    /* $("#TxtSearch").val('');*/
+    $("#Fdatevalidate").text("");
+    $("#Tdatevalidate").text("");
+
+    DailyVisitMonthWise(fDate, tDate);
+}
+
+$('#SearchBtn').on('click', function () {
+
+    var fDate = $("#FromDate").val();
+
+    var tDate = $("#ToDate").val();
+
+    if (fDate == "" || tDate == "") {
+
+
+        if (fDate == "" && tDate == "") {
+            $("#Fdatevalidate").text("From Date is Required");
+            $("#Tdatevalidate").text("To Date is Required");
+
+        }
+        else if (fDate == "" && tDate != "") {
+
+            $("#Fdatevalidate").text("From Date is Required");
+            $("#Tdatevalidate").text("");
+
+
+        }
+
+        else if (fDate != "" && tDate == "") {
+
+            $("#Fdatevalidate").text("");
+            $("#Tdatevalidate").text("To Date is Required");
+
+        }
+        else {
+            $("#Fdatevalidate").text("");
+
+            $("#Tdatevalidate").text("");
+            DailyVisitMonthWise(fDate, tDate);
+
+        }
+
+
+
+    }
+
+    else if (fDate != "" && tDate != "") {
+
+        if (fDate != null) {
+
+            $("#Fdatevalidate").text("");
+
+        } if (tDate != null) {
+
+            $("#Tdatevalidate").text("");
+
+        }
+        DailyVisitMonthWise(fDate, tDate);
+
+    }
+    //DailyVisitMonthWise(fDate, tDate);
+
+});
---
+++ PrakashCRM/obj/Release/AspnetCompileMerge/TempBuildDir/Scripts/reportjs/StockManagement.js
@@ -0,0 +1,191 @@
+﻿var apiUrl = $('#getServiceApiUrl').val() + 'SPReports/';
+
+$(document).ready(function () {
+    //var fDate = "";
+    //var tDate = "";
+
+    StockManagement();
+    //InquiryManagement(fDate, tDate);
+
+});
+
+function StockManagement() {
+    debugger
+
+    $.ajax({
+        url: '/SPReports/GetStockManagement',
+        type: 'GET',
+        contentType: 'application/json',
+        success: function (data) {
+            $('#tblStockManagement').empty();
+            var rowData = "";
+
+            if (data.length > 0) {
+                $.each(data, function (index, item) {
+                    const itemJson = JSON.stringify(item).replace(/"/g, '&quot;');
+                    rowData += `<tr data-item="${itemJson}"><td class="branchName"><a class="cursor-pointer">` + item.Branch + `</a></td><td>` + item.Inventory + `</td><td>` + item.Qty_on_Purch_Order + "</td><td>" + item.Qty_on_Sales_Order + "</td><td>" + item.Closing_Stock + "</td></tr>";
+                });
+            }
+            $('#tblStockManagement').append(rowData);
+
+        },
+
+    });
+
+}
+
+$(document).on('click', '.branchName', function () {
+    const itemJson = $(this).closest("tr").attr("data-item");
+    const item = JSON.parse(itemJson);
+    const branchName = item.Branch;
+    $("#SalesPerson").val(branchName);
+    BranchWiseProducts(branchName);
+});
+const ProductGroupsCode = {};
+function BranchWiseProducts(branchName) {
+    $.ajax({
+        url: '/SPReports/GetBranchWiseProducts',
+        type: 'GET',
+        contentType: 'application/json',
+        data: { BranchName: branchName },
+        success: function (data) {
+            $("#tbleStockProductManagement").empty();
+            $("#ftableBody").empty();
+            $('#DetailsModal').modal('show');
+            var rowData = "";
+            var rowData1 = "";
+
+            let groupData = {};
+            let totals = {};
+
+            if (data.BranchProductWise.length > 0) {
+                $.each(data.BranchProductWise, function (index, item) {
+                    let location = item.Location_Code;
+                    if (!groupData[location]) {
+                        groupData[location] = [];
+                    }
+                    if (item.Location_Total === "true") {
+                        totals[location] = item;
+                        //totals[item].push(item);
+                    }
+                    else {
+                        groupData[location].push(item);
+                    }
+
+                });
+
+                $.each(groupData, function (locations, records) {
+                    $.each(records, function (i, item) {
+                        var productId = `Product${i}SPRecs`;
+
+                        rowData += `<tr data-branchindex="${i}" class="branch-row">
+                       <td><a href="#" class="branch-toggle" data-target="${productId}"><i class="bx bx-plus-circle"></i></a></td>
+                       <td>${item.Description}</td><td>${item.Location_Code}</td><td>${item.Inventory}</td>
+                       <td>${item.Qty_on_Purch_Order}</td><td>${item.Qty_on_Sales_Order}</td>
+                       <td>${item.Closing_Stock}</td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       </tr>`;  
+
+                    });
+                  if (totals[locations]){
+                      let total = totals[locations];
+                      rowData += `<tr class="branch-row">
+               <td></td>
+               <td style="font-weight: bold;">Location Total</td>
+               <td>${total.Location_Code}</td><td>${total.Inventory}</td>
+               <td>${total.Qty_on_Purch_Order}</td><td>${total.Qty_on_Sales_Order}</td>
+               <td>${total.Closing_Stock}</td>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+               </tr>`;
+
+                    }
+                });
+
+                $("#tbleStockProductManagement").append(rowData);
+
+            }
+            if (data.BranchWiseTotalList.length > 0) {
+                $.each(data.BranchWiseTotalList, function (index, item) {
+
+                    rowData1 += `<tr>
+             <td></td>
+             <td style="font-weight: bold;">Total</td>
+             <td></td><td>${item.Inventory}</td>
+             <td>${item.Qty_on_Purch_Order}</td><td>${item.Qty_on_Sales_Order}</td>
+             <td>${item.Closing_Stock}</td>
+           <td>${item.Packing_Unit}</td><td>${item.Packing_MRP_Price}</td>
+           <td>${item.Expected_Receipt_Qty}</td><td>${item.Expected_Shipment_Qty}</td>
+         </tr>`;
+                });
+                $("#ftableBody").append(rowData1);
+            }
+
+            $("#tbleStockProductManagement").off('click.branch-toggle').on('click.branch-toggle', '.branch-toggle', function (e) {
+                e.preventDefault();
+
+                const $a = $(this);
+                const $icon = $a.find('i');
+                const $branchRow = $a.closest('tr.branch-row');
+                const bi = $branchRow.data('branchindex');
+                const branchName = data.BranchProductWise[bi].Location_Code;
+                const product = data.BranchProductWise[bi].Description;
+                const targetId = $a.data('target');
+
+                const $subRows = $branchRow.nextUntil('tr.branch-row');
+
+                if ($subRows.filter(':visible').length > 0) {
+                    $subRows.slideUp(300);
+                    $icon.removeClass('bx-minus-circle').addClass('bx-plus-circle');
+                } else {
+                    if ($subRows.find('tr[data-packing="true"]').length === 0) {
+                        $subRows.remove();
+                        loadProductPackingStyle($branchRow, branchName, product, bi, targetId);
+                    } else {
+                        $subRows.slideDown(300);
+                    }
+                    $icon.removeClass('bx-plus-circle').addClass('bx-minus-circle');
+                }
+            });
+        }
+    });
+}
+
+function loadProductPackingStyle($branchRow, branchName, product, bi, targetId) {
+    $.ajax({
+        url: `/SPReports/GetProductPackingStyle?BranchName=${encodeURIComponent(branchName)}&Product=${encodeURIComponent(product)}`,
+        type: 'GET',
+        success: function (packingstyle) {
+            let rowsHtml = "";
+
+            if (!packingstyle || packingstyle.length === 0) {
+                rowsHtml = `<tr data-packing="true" style="padding-left:20px;"><td colspan="11">No Packing Styles Found</td></tr>`;
+            } else {
+                packingstyle.forEach((ps, psi) => {
+                    const packingId = `${targetId}_Pack${psi}`;
+                    rowsHtml += `
+                        <tr data-packing="true" data-parent="${targetId}">
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                           
+                            <td>${ps.Inventory}</td><td>${ps.Qty_on_Purch_Order}</td>
+                            <td>${ps.Qty_on_Sales_Order}</td><td>${ps.Closing_Stock}</td>
+                            <td>${ps.Packing_Unit}</td><td>${ps.Packing_MRP_Price}</td>
+                            <td>${ps.Expected_Receipt_Qty}</td><td>${ps.Expected_Shipment_Qty}</td>
+                        </tr>`;
+                });
+            }
+
+            $branchRow.after(rowsHtml);
+        },
+        error: function () {
+            $branchRow.after('<tr data-packing="true"><td colspan="11" style="padding-left:20px;color:red;">Error loading packing styles</td></tr>');
+        }
+    });
+}
